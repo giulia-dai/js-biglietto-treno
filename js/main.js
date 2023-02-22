187b20cdab48d4c1e607a13d92229ce89f6eb2be
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-22 16:27:05 +0100

    variazione biglietto per minori e anziani

--- js/main.js
+++ js/main.js
@@ -15,18 +15,30 @@ sconto over 65: 40%
 let kilometri = parseInt(prompt("inserisci il numero dei Km da percorrere"));
 let età = parseInt(prompt("inserisci la tua età"));
 
-const biglietto = 
+const prezzoPerKm = 0.21;
+
+const prezzoIntero = kilometri*prezzoPerKm .toFixed(2);
+
+const prezzoMinorenni = (prezzoIntero / 100) * 20;
+
+const prezzoOver = (prezzoIntero / 100) * 40;
+
+
+
+
+
+
+
 
 let minorenne = 18;
 let over = 64;
 
 console.log (kilometri);
 console.log (età);
+console.log (prezzoIntero);
+console.log (prezzoMinorenni);
 
-if (età < minorenne)  {
 
-
-}
 
 
 
