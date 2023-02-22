6a7b4094fb05e5e7212bcaea0cbaddf2a5153999
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-22 16:50:08 +0100

    struttura in "if" e "if else"

--- js/main.js
+++ js/main.js
@@ -15,28 +15,35 @@ sconto over 65: 40%
 let kilometri = parseInt(prompt("inserisci il numero dei Km da percorrere"));
 let età = parseInt(prompt("inserisci la tua età"));
 
+let minorenne = 18;
+let over = 65;
+
 const prezzoPerKm = 0.21;
 
 const prezzoIntero = kilometri*prezzoPerKm .toFixed(2);
 
-const prezzoMinorenni = (prezzoIntero / 100) * 20;
+const scontoMinorenni = (prezzoIntero / 100) * 20;
+const totaleMinorenni = prezzoIntero - scontoMinorenni;
 
-const prezzoOver = (prezzoIntero / 100) * 40;
+const scontoOver = (prezzoIntero / 100) * 40;
+const totaleOver = prezzoIntero - scontoOver;
+
+if (età < minorenne) {
+  
+
+
+} else if (età > over){
+    console.log ("Il cliente ha diritto allo sconto Over");
+}
 
 
 
 
-
-
-
-
-let minorenne = 18;
-let over = 64;
 
 console.log (kilometri);
 console.log (età);
 console.log (prezzoIntero);
-console.log (prezzoMinorenni);
+
 
 
 
