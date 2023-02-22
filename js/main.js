9238489aef62b2206d5742f10a22736071be4791
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-22 15:37:50 +0100

    costo biglietto in base ai km percorsi(incompleto)

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 Quanti Km devi percorrere?
 
-Ogni Km costa 0.21€
-
 Quanti anni hai?
+
+Ogni Km costa 0.21€
 
 sconto minorenne: 20%
 
@@ -15,8 +15,20 @@ sconto over 65: 40%
 let kilometri = parseInt(prompt("inserisci il numero dei Km da percorrere"));
 let età = parseInt(prompt("inserisci la tua età"));
 
+const biglietto = 
+
+let minorenne = 18;
+let over = 64;
+
 console.log (kilometri);
 console.log (età);
+
+if (età < minorenne)  {
+
+
+}
+
+
 
 
 
