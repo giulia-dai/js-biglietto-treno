20e33b98add6f8d096fd8fb5d2701285c15fa3ba
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-02-22 15:23:48 +0100

    richiesta età e n. Km

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*
+
+Quanti Km devi percorrere?
+
+Ogni Km costa 0.21€
+
+Quanti anni hai?
+
+sconto minorenne: 20%
+
+sconto over 65: 40%
+
+*/
+
+let kilometri = parseInt(prompt("inserisci il numero dei Km da percorrere"));
+let età = parseInt(prompt("inserisci la tua età"));
+
+console.log (kilometri);
+console.log (età);
+
+
+
+
